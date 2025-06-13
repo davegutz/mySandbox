402987d383ca40e25ad31c338c46e564d3b5820b
Dave G <davegutz@alum.mit.edu>
2025-06-13 15:08:18 -0400

    clean

--- UTime/utime.cpp
+++ UTime/utime.cpp
@@ -5,6 +5,13 @@
 
 void utime()
 {
-    unsigned long long time_now = (unsigned long long) Time.now();  // UTC Zulu, s
-    printf("Hello\n");
+    // unsigned long long time_now = (unsigned long long) Time.now();  // UTC Zulu, s
+    time_t currentTime = time(nullptr); // Get current time
+    unsigned long now_ul = (unsigned long) (currentTime);
+    unsigned long long now_ull = (unsigned long long) (currentTime);
+    struct tm *gmt = gmtime(&currentTime);
+    printf("     %s\n", asctime(gmt));
+    printf("Current time %ld\n", currentTime);
+    printf("Current time %lu\n", now_ul);
+    printf("Current time %llu\n", now_ull);
 }
