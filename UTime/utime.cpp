537d36344ffdeab1ab61df6ebeaa7314a71acc7d
Dave G <davegutz@alum.mit.edu>
2025-06-13 15:31:30 -0400

    Update utime.cpp

--- UTime/utime.cpp
+++ UTime/utime.cpp
@@ -10,8 +10,15 @@ void utime()
     unsigned long now_ul = (unsigned long) (currentTime);
     unsigned long long now_ull = (unsigned long long) (currentTime);
     struct tm *gmt = gmtime(&currentTime);
+    unsigned long long time_now = now_ull;
+    unsigned long long inst_millis;          // millis offset to account for setup() time, ms
+    unsigned long long inst_time = time_now;            // UTC Zulu at instantiation, s
+    unsigned long long start = System.millis();
     printf("     %s\n", asctime(gmt));
-    printf("Current time %ld\n", currentTime);
-    printf("Current time %lu\n", now_ul);
-    printf("Current time %llu\n", now_ull);
+    printf("currentTime %ld\n", currentTime);
+    printf("now_ul      %lu\n", now_ul);
+    printf("now_ull     %llu\n", now_ull);
+    printf("time_now    %llu\n", time_now);
+    printf("inst_millis %llu\n", inst_millis);
+    printf("inst_time   %llu\n", inst_time);
 }
