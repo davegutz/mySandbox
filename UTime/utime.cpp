e133ce6863ba581611a41fc94da66149c31d02a8
Dave G <davegutz@alum.mit.edu>
2025-06-14 01:08:18 -0400

    debug

--- UTime/utime.cpp
+++ UTime/utime.cpp
@@ -13,21 +13,21 @@ long long get_current_time_ms()
 void utime()
 {
     // unsigned long long time_now = (unsigned long long) Time.now();  // UTC Zulu, s
-    time_t currentTime = time(nullptr); // Get current time
-    unsigned long now_ul = (unsigned long) (currentTime);
-    unsigned long long now_ull = (unsigned long long) (currentTime);
-    struct tm *gmt = gmtime(&currentTime);
+    time_t currentTime_ll = time(nullptr); // Get current time
+    unsigned long now_ul = (unsigned long) (currentTime_ll);
+    unsigned long long now_ull = (unsigned long long) (currentTime_ll);
+    struct tm *gmt = gmtime(&currentTime_ll);
     unsigned long long time_now = now_ull;
     unsigned long long inst_millis;          // millis offset to account for setup() time, ms
     unsigned long long inst_time = time_now;            // UTC Zulu at instantiation, s
-    unsigned long long start;
+    // unsigned long long start;
     long long milliseconds = get_current_time_ms();
     printf("     %s\n", asctime(gmt));
-    printf("currentTime %ld\n", currentTime);
-    printf("now_ul      %lu\n", now_ul);
-    printf("now_ull     %llu\n", now_ull);
-    printf("time_now    %llu\n", time_now);
-    printf("inst_millis %llu\n", inst_millis);
-    printf("inst_time   %llu\n", inst_time);
-    printf("milliseconds%lld\n", milliseconds);
+    printf("currentTime_ll %lld\n", currentTime_ll);
+    printf("now_ul         %lu\n", now_ul);
+    printf("now_ull        %llu\n", now_ull);
+    printf("time_now       %llu\n", time_now);
+    printf("inst_millis    %llu\n", inst_millis);
+    printf("inst_time      %llu\n", inst_time);
+    printf("milliseconds   %lld\n", milliseconds);
 }
