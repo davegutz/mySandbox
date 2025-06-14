40332bbb4acd4a2301d8a6debd1cb7e7572037de
Dave G <davegutz@alum.mit.edu>
2025-06-14 04:05:52 -0400

    Update utime.cpp

--- UTime/utime.cpp
+++ UTime/utime.cpp
@@ -21,6 +21,8 @@ void utime()
     unsigned long long sample_time_ib_ll = 0ULL;  // Exact moment of selected Ib sample, ms
     unsigned long long now_ull = 0ULL;  // Time at sample, ms
     
+    printf("\n\ninst_millis_ll, ms   sample_time_ib_ll, ms   now_ull, ms        asctime\n");
+
     for ( int i=0; i<3; i++)
     {
         readable_time_ull = time(nullptr);  // Get current time
@@ -29,10 +31,12 @@ void utime()
         sample_time_ib_ll = get_current_time_ms();
         now_ull = sample_time_ib_ll - inst_millis_ll + inst_time_ul*1000;
 
-        printf("\n\n%s\n", asctime(gmt_readable));
-        printf("inst_millis_ll  %lld ms\n", inst_millis_ll);
-        printf("now_ull         %llu     s\n", now_ull);
+        printf("%lld        ", inst_millis_ll);
+        printf("%lld           ", sample_time_ib_ll);
+        printf("%llu         ", now_ull);
+        printf("%s", asctime(gmt_readable));
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
+    printf("inst_millis_ll, ms   sample_time_ib_ll, ms   now_ull, ms        asctime\n\n");
 }
