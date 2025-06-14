17c37cbe04474bda2d58b9f675106f04b0e9f4a2
Dave G <davegutz@alum.mit.edu>
2025-06-14 02:58:46 -0400

    Update utime.cpp

--- UTime/utime.cpp
+++ UTime/utime.cpp
@@ -27,12 +27,12 @@ void utime()
     unsigned long long inst_time_ull = time_now_ull;         // UTC Zulu at instantiation, s
 
     printf("%s\n", asctime(gmt));
-    printf("currentTime_ll    %lld\n", currentTime_ll);
-    printf("milliseconds_ll   %lld\n", milliseconds_ll);
-    printf("start_ull         %llu\n", start_ull);
-    printf("now_ul            %lu\n", now_ul);
-    printf("now_ull           %llu\n", now_ull);
-    printf("time_now_ull      %llu\n", time_now_ull);
-    printf("inst_millis_ull   %llu\n", inst_millis_ull);
-    printf("inst_time_ull     %llu\n\n\n", inst_time_ull);
+    printf("currentTime_ll    %lld     s\n", currentTime_ll);
+    printf("milliseconds_ll   %lld ms\n", milliseconds_ll);
+    printf("start_ull         %llu ms\n", start_ull);
+    printf("now_ul            %lu     s\n", now_ul);
+    printf("now_ull           %llu     s\n", now_ull);
+    printf("time_now_ull      %llu     s\n", time_now_ull);
+    printf("inst_millis_ull   %llu ms\n", inst_millis_ull);
+    printf("inst_time_ull     %llu     s\n\n\n", inst_time_ull);
 }
